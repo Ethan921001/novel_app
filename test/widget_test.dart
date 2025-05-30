cdde897ae8d1e909121cb35df09dd3f91c6bf8a2
Ethan921001 <king6969003@gmail.com>
2025-05-30 15:17:23 +0800

    change tts file name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:novel_app/main.dart';
+import 'package:novel_app/tts.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
