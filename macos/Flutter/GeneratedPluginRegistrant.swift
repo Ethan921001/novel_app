ab27866aa8a8e71c9304e90093f33b12fa5a938f
Ethan921001 <king6969003@gmail.com>
2025-06-06 15:33:09 +0800

    add test to image.dart

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_stable_diffusion_core_ml
 import flutter_tts
+import platform_object_channel_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterStableDiffusionCoreMlPlugin.register(with: registry.registrar(forPlugin: "FlutterStableDiffusionCoreMlPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  PlatformObjectChannelFoundationPlugin.register(with: registry.registrar(forPlugin: "PlatformObjectChannelFoundationPlugin"))
 }
