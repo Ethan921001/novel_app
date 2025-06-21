f09c71ae43661a68df2fffd0d4aaf50b24b166f3
Ethan921001 <king6969003@gmail.com>
2025-06-21 14:16:56 +0800

    fix .lock file

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,14 @@ import Foundation
 
 import flutter_stable_diffusion_core_ml
 import flutter_tts
-import platform_object_channel_foundation
 import path_provider_foundation
+import platform_object_channel_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterStableDiffusionCoreMlPlugin.register(with: registry.registrar(forPlugin: "FlutterStableDiffusionCoreMlPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
-  PlatformObjectChannelFoundationPlugin.register(with: registry.registrar(forPlugin: "PlatformObjectChannelFoundationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PlatformObjectChannelFoundationPlugin.register(with: registry.registrar(forPlugin: "PlatformObjectChannelFoundationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
